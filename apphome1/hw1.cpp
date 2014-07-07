238470bc5ae18a7ce44eb183e664d7c5239afab2
acersanya <olexander.halii@gmail.com>
2014-07-07 13:18:27 +0300

    added cooments

--- apphome1/hw1.cpp
+++ apphome1/hw1.cpp
@@ -18,24 +18,29 @@ bool checker (double a, double b  , double c) {  // function which checks НЕ(Ац 
 
 void Calcute_func (double X_start , double X_end , double dx , double a, double b, double c , bool checker){ // if FALSE - Integer if TRUE - Real number
 	int counter =1; // counter
-	double tmp=0;
+	double tmp=0; //змінна де зберігаються проміжні обчислення
 
-	for(double i = X_start; i< X_end; i=i+dx){
+	for(double i = X_start; i< X_end; i=i+dx){ 
 		cout<<counter<<")";
 
-		if( (i<3) && (b!=0)){
+		if( (i<3) && (b!=0)){  // перший випадок  
 			cout<<"ax^2-bx+c:  \t";  
 			tmp = a* pow(i,2)-b*i+c; 
-			if( checker == true) {
+			if( checker == true) { // якщо функцію вернула true то повертаємо Дійсне число 
 				cout<<tmp <<" \t";
 				cout<<"TRUE"<<endl;}
-			else { 
+			else {  // якщо ні то цілу частину
 				cout<<(int)tmp<<" \t";
 				cout<<"FALSE"<<endl;}} 
 
 
-		else if( (i>3) && b==0 ){
+		else if( (i>3) && b==0 ){ // другий випадок
 			cout<<"x-a/x-c:  \t";
+
+			if(i-c ==0){ // Divizion by zero
+				continue;}
+
+
 			tmp= (i-a)/(i-c);
 			if (checker==true){
 
@@ -53,6 +58,11 @@ void Calcute_func (double X_start , double X_end , double dx , double a, double 
 		else
 		{   
 			cout<<"x/c: \t";
+
+			if(c==0){
+				cout<<"Divizion by zero"; // ділення на ноль
+				break;}
+
 			tmp=i/c;	
 			if(checker==true){
 
@@ -67,7 +77,7 @@ void Calcute_func (double X_start , double X_end , double dx , double a, double 
 		}
 
 
-		counter++;
+		counter++; // лічильник коли виводжу в консаоль
 	}
 
 }
@@ -76,9 +86,9 @@ void Calcute_func (double X_start , double X_end , double dx , double a, double 
 int main(){
 
 	setlocale( LC_CTYPE,"Russian" ); 
-	double X_start =0 , X_end=0, dx=0;
+	double X_start =0 , X_end=0, dx=0; //початкова ініціалізація змінних
 	double a=0,b=0,c=0;
-	string answer;
+	string answer; // в циклі do - while якщо користувач сказав так, повторюємо дію знову якщо ні то вихід
 	bool check_answer;
 
 	do {
@@ -88,18 +98,18 @@ one:
 
 		cout<<"Enter X_start , X_end , dx:"<<endl;
 
-		while(!((cin >> X_start) && (cin>> X_end) && (cin>>dx))){
+		while(!((cin >> X_start) && (cin>> X_end) && (cin>>dx))){ // перевірка на валідність, лише чисельні значення дозволені
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid input. ONLY DIGITS ARE ALLOWED ";
     }
-		if(X_end<X_start) { cout<<"try again X_end > X_start"<<endl; goto one;}
+		if(X_end<X_start) { cout<<"try again X_end > X_start"<<endl; goto one;} // також мусить виповнятися умова , X_start > X_end
 
 		
 
 		cout<<"Enter: a,b,c"<<endl;
 	
-		while(!((cin >> a) && (cin>> b) && (cin>>c))){
+		while(!((cin >> a) && (cin>> b) && (cin>>c))){ // перевірка на валідність вводу.
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         cout << "Invalid input. ONLY DIGITS ARE ALLOWED ";
@@ -107,12 +117,12 @@ one:
 
 
 
-		bool check_in  = checker(a,b,c); // calculates the task  HE(Ac or Bc) and (Ac mod2 Bc)
+		bool check_in  = checker(a,b,c); // calculates the task  HE(Ac or Bc) and (Ac mod2 Bc) тут ми перевіряємо нашу умову. якщо FALSE - ціла частина якщо  TRUE то дійсна частина числа
 
 		cout<<check_in<<endl;
-		Calcute_func(X_start, X_end, dx , a ,b,c , check_in);	
+		Calcute_func(X_start, X_end, dx , a ,b,c , check_in);	 // значення функції на нашому інтервалі
 
-		cout<<"You want to continue?";
+		cout<<"You want to continue?"; // якщо користувач хоче продовжити пише yes якщо ні - no
 
 once_again: // go to ^_^
 		cin>>answer; 
